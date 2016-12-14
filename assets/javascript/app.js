a7aa313fb4b7c5f668b29a2c55ef225179afe275
mchogan7 <mills.c.hogan@gmail.com>
2016-12-13 22:03:40 -0600

    branch merge test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,25 @@
  var messThingsUp = true
 
 
-function thisDoesNothing(){
+function thisDoesSomething(){
 	if(nothing){
-		console.log('nothing')
+		console.log('nothing and something')
 	}
 }
+
+Terrible changes here.
 //Make a giant mess here.
- josh_branch
+anch
 
 //first test of a push to the josh branch
 
-f;lasd jfpaosdnfa;sldfnkasdf a;sldfkjas;d fasdlfkjasdf 
+sldfnkasdf a;sldfkjas;d fasdlfkjasdf 
  ec4123fbc1b9c948f78921f0fd65776b94bc6641
 
-var aTerribleList = ['stuff', 'thing', 15]
+var aTerribleList = ['thing', 15]
 
 //My test function -Mills
-
+Just gonna mess things up now.
 
 console.log (aTerribleList)
 
