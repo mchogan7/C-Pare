f1b8aa0c7aac388344cb05d1237b66a5c4e7440d
mchogan7 <mills.c.hogan@gmail.com>
2016-12-17 11:13:55 -0600

    updated stock search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,54 +11,71 @@ var tickerSymbol
 
 //Quandle AJAX Call - Work in progress
 //quandle key EDWEb1oyzs8FrfoFyG1u
+function stockAJAX() {
+    var queryURL = "https://www.quandl.com/api/v3/datasets/WIKI/" + tickerSymbol + ".json?column_index=4&start_date=2015-01-01&end_date=2016-12-14&collapse=daily&api_key=EDWEb1oyzs8FrfoFyG1u";
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
 
-var queryURL = "https://www.quandl.com/api/v3/datasets/WIKI/aapl.json?column_index=4&start_date=2015-01-01&end_date=2016-12-14&collapse=daily&api_key=EDWEb1oyzs8FrfoFyG1u";
-$.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-    //console.log(response)
+        var stocksChartData = []
 
-})
+        for (var i = 0; i < response.dataset.data.length; i++) {
+            stocksChartData.push(response.dataset.data[i][1])
+        }
+        console.log(stocksChartData)
+
+        var stockDataObject = {
+            label: "I will remove this",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: stocksChartData,
+            spanGaps: false,
+        }
+
+
+    })
+}
 
 //End of Quandle AJAX Call
 
 //Testing function
-tickerConverter('apple')
+//tickerConverter('apple')
 
 //Ticker Converter Function
-	function tickerConverter(userSearch) { 
-        $.ajax({
-             success : function (response)
-              {
-                tickerSymbol = response.ResultSet.Result[0].symbol
+function tickerConverter(userSearch) {
+    $.ajax({
+        success: function(response) {
+            tickerSymbol = response.ResultSet.Result[0].symbol
                 //We will have to put the function for the next AJAX request here.
                 //This is to avoid any async issues.
-
-                //log to show it is working.	
-                console.log(tickerSymbol)
-                },
-            type: "GET",
-            url: "http://d.yimg.com/autoc.finance.yahoo.com/autoc",
-            data: {
-                query: userSearch,
-                region: 'US',
-                lang: 'en-US'
-            },
-            dataType: "jsonp"
-           })
-    }
-    //
-
-
-
-
-
-
-
-
-
-
-
-
-
+            stockAJAX();
+            //log to show it is working.	
+            console.log(tickerSymbol)
+            console.log(response)
+        },
+        type: "GET",
+        url: "http://d.yimg.com/autoc.finance.yahoo.com/autoc",
+        data: {
+            query: userSearch,
+            region: 'US',
+            lang: 'en-US'
+        },
+        dataType: "jsonp"
+    })
+}
 
 
 //Chart Stuff Below here
@@ -69,7 +86,7 @@ var ctx = $("#mainChart");
 
 //Place holder data
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
     datasets: [{
         label: "I will remove this",
         fill: false,
@@ -89,7 +106,7 @@ var data = {
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [65, 59, 80, 81, 56, 55, 40, 90],
         spanGaps: false,
     }]
 };
@@ -108,3 +125,12 @@ var mainChart = new Chart(ctx, {
         }
     }
 });
+
+
+//UI and DOM stuff below here:
+
+$('#compare').on('click', function(e) {
+    e.preventDefault();
+    var userInput = $('#query-input').val().trim();
+    tickerConverter(userInput)
+})
